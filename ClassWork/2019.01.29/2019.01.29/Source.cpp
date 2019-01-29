b379d8f9565ea00f6ed9082c7e4ea5e7056a6693
Pavlo Maistruk <PavlMais@step.itstep.rv.ua>
2019-01-29 13:12:38 +0200

    new task

---
+++ ClassWork/2019.01.29/2019.01.29/Source.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+inline int getSeconds(int hours , int minutes  = 0, int seconds = 0) {
+	if(hours > 0 && hours < 25 && minutes > 0 && minutes < 61 && seconds > 0 && seconds < 61)
+		return hours * 60 * 60 + minutes * 60 + seconds;
+	else {
+		cout << "Invalid args!\n\n";
+		return 0;
+	}
+}
+
+
+int main() {
+	int a, b, c;
+	cout << "Enter (hours minutes seconds):\n";
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	cout << "Seconds: " << getSeconds(a,b,c) << "\n\n";
+
+	system("pause");
+	return 0;
+}
